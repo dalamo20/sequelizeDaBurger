834616d49ad469e28e005c074b31d6d11d16dcd0
Daniel A <dalamo90@gmail.com>
2018-06-05 18:44:42 -0500

    psuedocode

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,7 @@
 
 // controlls all functions for one tables routes
 var express = require('express');
-var burger = require("../models/burgers");
+var db = require("../models/burgers");
 // all api routes
 var router = express.Router();
 
@@ -11,26 +11,46 @@ var router = express.Router();
 
 // -- get route for the root route using express
 // -- this route needs to connect to the burger.js (models) to select all the data
+// router.get("route", function)
 router.get("/", function (req, res) {
-    burger.selectAll(function (data) {
+    //findAll is sequelize syntax 
+    db.Burger.findAll(function (data) {
         // this cb `function (data)` becomes cb in burger.js
         // we are passing this callback function to burger.selectAll
         // console.log(data);
+        //res.render = result of the findAll function
+        //res.render("route", {always an object - table name: what you want back})
         res.render("index", { burgers: data });
     });
 });
 
+//router.post("route", function)
 router.post("/burgers/insert", function (req, res) {
     console.log(req.body);
-
-    burger.insertOne(req.body, function (data) {
+    //.create() is sequelize function to POST new info into database
+    db.Burger.create(
+        {//only specify columns you need to create
+        burger_name: req.body.burger,
+        //BOOLEANS you want to give default value to
+        devoured: 0
+     }).then(function (data) {
+         //reloads page
         res.redirect("/");
     });
 });
 
+
+//router.put("route", function)
+//route/: <-- the : allows you to create a URL with 
 router.put("/burgers/:id", function (req, res) {
     // var updateBurger = { burger_name: "" };
-    burger.updateOne({ devoured: 1 }, req.params.id, function (data) {
+    db.Burger.update({
+        devoured: 0
+     }, {
+         where: {
+             id: req.params.id
+         }
+     }).then(function (data) {
         res.sendStatus(200);
     });
 });
